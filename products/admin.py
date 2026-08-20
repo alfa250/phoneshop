d9b514e0c7675b8dc58b9596ec442e7673817b74
alfa250 <alfredaondofa@gmail.com>
2026-08-20 07:21:04 +0100

    improve django admin for products and orders

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,84 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Order, OrderItem
 
-# Register your models here.
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'sale_price',
+        'is_sale',
+        'stock',
+    )
+
+    list_filter = (
+        'category',
+    )
+
+    search_fields = (
+        'name',
+        'description',
+    )
+
+    list_editable = (
+        'stock',
+        'is_sale'
+    )
+
+    def current_price(self, obj):
+        return obj.current_price
+
+    current_price.short_description = 'Current Price'
+
+
 admin.site.register(Category)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = (
+        'subtotal_display',
+    )
+    def subtotal_display(self, obj):
+        if obj.quantity is None or obj.price is None:
+            return '-'
+
+        return obj.quantity * obj.price
+
+    subtotal_display.short_description = 'Subtotal'
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'order_number',
+        'full_name',
+        'user',
+        'date_created',
+        'status',
+        'total',
+    )
+
+    list_filter = (
+        'status',
+        'date_created',
+    )
+
+
+    search_fields = (
+        'order_number',
+        'full_name',
+        'phone',
+        'user__username',
+    )
+
+    ordering = (
+        '-date_created',
+    )
+
+    inlines = (
+        OrderItemInline,
+    )
+
